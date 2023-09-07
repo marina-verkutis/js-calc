042b1f9f54622d26c3583f7a81d583d83770a2f1
Marina Verkutis <marina.corpwork@gmail.com>
2023-09-07 19:15:52 +0400

    Add perсent

---
+++ calc.js
@@ -0,0 +1,98 @@
+let num1 = '';
+let num2 = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/', '%'];
+
+const out = document.querySelector('.calc-screen p');
+
+function clearAll() {
+    num1 = '';
+    num2 = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+document.querySelector('.ac').onclick = clearAll;
+
+function deleteLast() {
+    console.log('delete');
+    if(num2 !== '') {
+        num2 = num2.slice(0, -1);
+        out.textContent = num2;
+    }
+    else if (num1 !== '') {
+        num1 = num1.slice(0, -1);
+        out.textContent = num1;
+        console.log('slice 1');
+    }
+    else {}
+}
+document.querySelector('.delete').onclick =  deleteLast;
+
+document.querySelector('.buttons').onclick = (event) => {
+    if(!event.target.classList.contains('btn')) return;
+    if(event.target.classList.contains('ac')) return;
+    if(event.target.classList.contains('delete')) return;
+
+    out.textContent = '';
+    const key = event.target.textContent;
+
+    if(digit.includes(key)) {
+        if(num2 === '' && sign === '') {
+            num1 += key;
+            console.log(num1, num2, sign);
+            out.textContent = num1;
+        }
+        else if (num1 !== '' && num2 !== '' && finish) {
+            num2 = key;
+            finish = false;
+            out.textContent = num2;
+        }
+        else {
+            num2 += key;
+            out.textContent = num2;
+        }
+        console.log(num1, num2, sign);
+        return;
+    }
+
+    if(action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.log(num1, num2, sign);
+        return;
+    }
+
+    if(key === '=') {
+        if(num2 === '') num2 = num1;
+        switch (sign) {
+            case "+":
+                num1 = (+num1) + (+num2);
+                break;
+            case "-":
+                num1 = num1 - num2;
+                break;
+            case "X":
+                num1 = num1 * num2;
+                break;
+            case "/":
+                if(num2 === '0') {
+                    out.textContent = 'ERROR!';
+                    num1 = '';
+                    num2 = '';
+                    sign = '';
+                    return;
+                }
+                num1 = num1 / num2;
+                break;
+            case "%":
+                num1 = (num1 / 100) * num2;
+                break;
+        }
+        finish = true;
+        out.textContent = num1;
+    }
+}
